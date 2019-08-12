4864923163303648f4b058a2269dd9db7f319ef0
tatatakky <s1240232@gmail.com>
2019-08-13 00:29:04 +0900

    [add] either in finishOneUserProcess function

--- src/main/scala/UserList.scala
+++ src/main/scala/UserList.scala
@@ -1,21 +1,23 @@
 case class User(name: String, id: Int)
 object UserList {
 
+  sealed trait UserList[+_]
+  case object UserNil extends UserList[Nothing]
+  case class UserCons(head: User, tail: UserList[User]) extends UserList[User]
+
   def apply(as: User*): UserList[User] =
     if (as.isEmpty) UserNil
     else UserCons(as.head, apply(as.tail: _*))
 
-  def finishOneUserProcess(ul: UserList[User]): UserList[User] = ul match {
-    case UserNil => sys.error("User Nothing")
-    case UserCons(_, t) => t
+  def finishOneUserProcess(ul: UserList[User]): Either[Error, UserList[User]] = ul match {
+    case UserNil => Left(UserNotFound)
+    case UserCons(_, t) => Right(t)
   }
-
 }
 
+sealed trait Error
+case object UserNotFound extends Error
 
-sealed trait UserList[+_]
-case object UserNil extends UserList[Nothing]
-case class UserCons(head: User, tail: UserList[User]) extends UserList[User]
 
 object Main2 {
 
@@ -23,7 +25,10 @@ object Main2 {
 
   def main(args: Array[String]): Unit = {
     val userList: UserList[User] = UserList(User("bar,", 1), User("foo", 2), User("baz", 3))
-    val updateUserList: UserList[User] = finishOneUserProcess(userList)
-    println(updateUserList)
+    val userList2: UserList[User] = UserList()
+    finishOneUserProcess(userList) match {
+      case Right(value) => println(value)
+      case Left(error)  => println(error)
+    }
   }
 }
